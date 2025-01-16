18888e861e7f2d9e6656ce17d2f91a5d5c7edabd
Ray Anthony <16364682+Ray-Anthony@users.noreply.github.com>
2025-01-16 12:00:46 -0600

    Update phrases and apply minor responsive tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -16,14 +16,14 @@ export default function Page() {
       "Are you sure?",
       "What if I asked really nicely?",
       "Pretty please",
-      "With a chocolate rice cake on top",
-      "What about a matcha frostie",
-      "PLEASE POOKIE",
+      "With strawberry cake on top",
+      "What about a hello kitty cake?",
+      "PLEASE :(",
       "But :*(",
       "I am going to die",
       "Yep im dead",
-      "ok ur talking to nathan's ghost",
-      "please babe",
+      "ok ur talking to my ghost",
+      "please jabroni",
       ":((((",
       "PRETTY PLEASE",
       "Estoy muerto",
@@ -34,11 +34,17 @@ export default function Page() {
   };
 
   return (
-    <div className="-mt-16 flex h-screen flex-col items-center justify-center">
+    <div
+      className={`-mt-16 flex h-screen flex-col items-center justify-center px-5 md:px-0 ${
+        yesPressed ? "gap-10" : ""
+      }`}
+    >
       {yesPressed ? (
         <>
           <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-          <div className="my-4 text-4xl font-bold">WOOOOOO!!! I love you pookie!! ;))</div>
+          <div className="my-4 text-2xl font-bold">
+            WOOOOOO!!! I love you!! <span className="text-3xl">🥹</span>
+          </div>
         </>
       ) : (
         <>
@@ -46,7 +52,9 @@ export default function Page() {
             className="h-[200px]"
             src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
           />
-          <h1 className="my-4 text-4xl">Will you be my Valentine?</h1>
+          <h1 className="my-4 text-3xl text-center">
+            Will you be my Valentine?
+          </h1>
           <div className="flex items-center">
             <button
               className={`mr-4 rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700`}
